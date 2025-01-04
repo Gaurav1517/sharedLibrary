bd1b1710a3e65dc9b7c3540fecaf076011644bd0
Gaurav Chauhan <145908403+Gaurav1517@users.noreply.github.com>
2025-01-04 17:16:21 +0530

    Update tag_docker_image.groovy

--- vars/tag_docker_image.groovy
+++ vars/tag_docker_image.groovy
@@ -1,3 +1,7 @@
 def call(String JOB_NAME, int BUILD_NUMBER, String docker_user) {
-  sh "docker tag ${JOB_NAME.toLowerCase()}:v${BUILD_NUMBER} ${docker_user}/${JOB_NAME.toLowerCase()}:latest"
+    sh """
+        docker tag \
+        ${JOB_NAME.toLowerCase()}:v${BUILD_NUMBER} \
+        ${docker_user}/${JOB_NAME.toLowerCase()}:latest
+    """
 }
